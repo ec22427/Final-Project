53387852836778db18a30a43eeb3ad55bdf5644a
ec22427 <ec22427@qmul.ac.uk>
2024-01-19 02:19:40 +0000

    second

--- Final project.py
+++ Final project.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load the dataset
 df = pd.read_csv("Kpop 4th gen Sales - Sheet1.csv")
@@ -8,4 +10,10 @@ df['sales'] = pd.to_numeric(df['sales'].str.replace(',', ''), errors='coerce')
 
 # Display basic information about the dataset
 print(df.info())
+
+print(df.head())  # Display the first few rows of the DataFrame
+print(df['sales'].unique())  # Check unique values in the 'sales' column
+print(df['peak_chart'].unique())  # Check unique values in the 'peak_chart' column
+
+
 
